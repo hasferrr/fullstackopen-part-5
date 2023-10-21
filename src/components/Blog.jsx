164564cdebfab3ae3e70f0a995fa6e22ad4e3417
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-10-22 00:03:25 +0700

    finish 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import './Blog.css'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div className='blog'>
+      <div>
+        {blog.title}
+        <button onClick={() => setVisible(!visible)}>
+          {visible ? 'hide' : 'show'}
+        </button>
+      </div>
+      <div style={{ display: visible ? '' : 'none' }}>
+        <a href={blog.url}>{blog.url}</a>
+        <div>
+          likes {blog.likes}
+          <button>like</button>
+        </div>
+        <div>by {blog.author}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
