2f02dd0bef45af938914f70a53a4c96177e7dbd4
hasferrr <79697518+hasferrr@users.noreply.github.com>
2023-11-04 17:20:31 +0700

    finish 5.14

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('title and author are shown', () => {
+describe('<Togglable />', () => {
+  let container
+
   const blog = {
     user: 1,
     likes: 10,
@@ -14,14 +17,34 @@ test('title and author are shown', () => {
 
   const mockHandler = jest.fn()
 
-  render(
-    <Blog
-      blog={blog}
-      updateBlog={mockHandler}
-      deleteBlog={mockHandler}
-    />
-  )
+  beforeEach(() => {
+    container = render(
+      <Blog
+        blog={blog}
+        updateBlog={mockHandler}
+        deleteBlog={mockHandler}
+      />
+    ).container
+  })
 
-  const element = screen.getByText(`${blog.title} by ${blog.author}`)
-  expect(element).toBeDefined()
+  test('title and author are shown', () => {
+    const element = screen.getByText(`${blog.title} by ${blog.author}`)
+    expect(element).toBeDefined()
+  })
+
+  test('URL and likes are shown after show button has been clicked', async () => {
+    const element = screen.getByText(`${blog.title} by ${blog.author}`)
+    const button = element.querySelector('button')
+
+    const user = userEvent.setup()
+    await user.click(button)
+
+    const div = container.querySelectorAll('div')
+    const anchorUrl = screen.getByText(blog.url)
+    const likeElement = screen.getByText(`likes ${blog.likes}`)
+
+    expect(div[1]).not.toHaveStyle({ display: 'none' })
+    expect(anchorUrl).toBeDefined()
+    expect(likeElement).toBeDefined()
+  })
 })
